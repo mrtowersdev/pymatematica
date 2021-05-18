b2764fb5869c798c6de21d6771a3a6ffdbe64c30
mrtowersdev <daniel.ttorres@gmail.com>
2021-05-17 23:49:18 -0300

    Adicionando metodos para geração de array de permutação simples e com repetição

--- analise_combinatoria/permutacao.py
+++ analise_combinatoria/permutacao.py
@@ -1,15 +1,60 @@
 from analise_combinatoria.fatorial import fatorial
 
 
-def simples(n):
+def calculo_permutacao_simples(n):
     pn = fatorial(n)
     return pn
 
 
-def com_repeticao(n, k):
+def calculo_permutacao_com_repeticao(n, k):
     nk = 1
     for i in k:
         nk = nk * fatorial(i)
     pn = fatorial(n)/nk
 
     return pn
+
+
+def permutacao_simples(lst):
+    p = [0 for x in range(len(lst))]
+
+    def imprime():
+        result = []
+        for i in range(len(p)):
+            result.append(p[i])
+        print(result)
+
+    def permuta(lst, n):
+        if n == len(lst):
+            imprime()
+        else:
+            for i in range(len(lst)):
+                achou = False
+                for j in range(n):
+                    if p[j] == lst[i]:
+                        achou = True
+                if not achou:
+                    p[n] = lst[i]
+                    permuta(lst, n+1)
+
+    permuta(lst, 0)
+
+
+def permutacao_com_repeticao(lst):
+    p = [0 for x in range(len(lst))]
+
+    def imprime():
+        result = []
+        for i in range(len(p)):
+            result.append(p[i])
+        print(result)
+
+    def permuta(lst, n):
+        if n == len(lst):
+            imprime()
+        else:
+            for i in range(len(lst)):
+                p[n] = lst[i]
+                permuta(lst, n+1)
+
+    permuta(lst, 0)
