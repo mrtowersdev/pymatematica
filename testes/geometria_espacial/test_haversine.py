800e09313bdca96a561ec1bd6e4d8149ac61a5de
mrtowersdev <daniel.ttorres@gmail.com>
2021-06-26 18:09:46 -0300

    adicionando teste de calculo de distancia entre dois ponto - geometria espacial

--- testes/geometria_espacial/test_haversine.py
+++ testes/geometria_espacial/test_haversine.py
@@ -4,10 +4,10 @@ from calculos.geometria_espacial.haversine import distance_kilometers, distance_
 
 class CalculoDistanciaTeste(unittest.TestCase):
     def test_distancia_kilometros(self):
-        self.assertEqual(distance_kilometers(-23.542987, -46.344314, -23.544095, -46.343032), 0.11163634749890428)
+        self.assertEqual(distance_kilometers(0, 0, 0, 0), 0.11163634749890428)
 
     def test_distancia_metros(self):
-        self.assertEqual(distance_meters(-23.542987, -46.344314, -23.544095, -46.343032), 111.63634749890427)
+        self.assertEqual(distance_meters(0, 0, 0, 0), 111.63634749890427)
 
 
 if __name__ == '__main__':
